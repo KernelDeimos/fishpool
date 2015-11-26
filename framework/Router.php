23f3c55b76c0b522edcb453a5e3612683118b716
Eric Dub&eacute <eric.alex.dube@gmail.com>
2015-11-26 12:01:46 -0500

    Create the Request class

---
+++ framework/Router.php
@@ -0,0 +1,62 @@
+<?php
+
+class Request {
+	private $pathArray;
+
+
+	function __construct($requestString) {
+		// Parse URL codes in the string
+		$requestString = urldecode($requestString);
+		// Generate path array from string (use delimiter of '/' or '\')
+		$pathArray = preg_split('(/+,\\+)', $requestString);
+		// Set instance variable
+		$this->pathArray = $pathArray;
+	}
+
+	/**
+	 * Get the page name
+	 *
+	 * Returns the very first item in the request's path.
+	 * Returns an empty string or a string containing the
+	 * first item.
+	 *
+	 * Example:
+	 * pageName/2/some-item
+	 * calling thus function would return pageName
+	 *
+	 * @return string the page name
+	*/
+	function get_page() {
+		return $pathArray[0];
+	}
+
+	/**
+	 * Get a Parameter
+	 *
+	 * Obtains a deeper item in the URL path than
+	 * the root item, using indexes starting at
+	 * zero. Returns null if parameter does not exist.
+	 *
+	 * Example:
+	 * pageName/2/some-item
+	 * the index 0 returns '2',
+	 * the index 1 returns 'some-item'
+	 *
+	 * @param ind the index of the parameter to retrieve
+	 * @return mixed the parameter's value, or null
+	 */
+	function get_parameter($ind) {
+		// Store pathArray in a local variable
+		$pathArray = $this->pathArray;
+		// Start at second value of path array
+		$ind += 1;
+		// Determine if a parameter exists here (AND IS NOT NULL!)
+		if (isset($pathArray)) {
+			// Return item if it exists
+			return $this->pathArray[$ind];
+		} else {
+			// Return null if we didn't find anything
+			return null;
+		}
+	}
+}
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// Define the WEB_PATH variable
+// (allows HTML pages to refer to specific locations more easily)
+{
+	// Create a pattern for the current file-system path
+	$pattern = '/^'.preg_quote($_SERVER['DOCUMENT_ROOT'],'/').'/';
+	// Replace the match of this pattern with the host name (domain)
+	$webpath = "http://".$_SERVER['HTTP_HOST'].preg_replace($pattern,'',getcwd());
+	define('WEB_PATH',$webpath);
+}
+
+/**
+ * Main website function
+ *
+ * This function determins the request being made, and routes
+ * the request to  particular module.
+ */
+function main() {
+	$requestString
+}
+
+try {
+	main();
+} catch (Exception $e) {
+	//
+}
+
+echo "The query string is: " . $_SERVER['QUERY_STRING'];
+echo "<br />";
+echo "The RI is: " . $_GET['ri'];

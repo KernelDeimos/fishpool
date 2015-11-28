434299620978dd7ef052a69fa4df43321d92a8c5
Eric Dub&eacute <eric.alex.dube@gmail.com>
2015-11-28 00:06:55 -0500

    Create tables for groups, projects, and files

--- setup/accounts_table.sql
+++ setup/accounts_table.sql
@@ -3,16 +3,16 @@ CREATE SCHEMA FishpoolDB;
 use FishpoolDB;
 
 CREATE TABLE accounts (
-        account_id MEDIUMINT NOT NULL AUTO_INCREMENT,
-        name VARCHAR(40),
-        username VARCHAR(40),
-        pass_hash CHAR(64),
-        pass_salt CHAR(32),
-        reset_email TEXT,
-        attempts TINYINT NOT NULL DEFAULT 0,
-        pwd_reset VARCHAR(12) NOT NULL default 'OK',
-        activation CHAR(8) NOT NULL default 'OK',
-        last_attempt datetime NOT NULL default '0000-00-00 00:00:00',
-        date_created datetime NOT NULL default '0000-00-00 00:00:00',
-        PRIMARY KEY (account_id)
+	account_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(40),
+	username VARCHAR(40),
+	pass_hash CHAR(64),
+	pass_salt CHAR(32),
+	reset_email TEXT,
+	attempts TINYINT NOT NULL DEFAULT 0,
+	pwd_reset VARCHAR(12) NOT NULL default 'OK',
+	activation CHAR(8) NOT NULL default 'OK',
+	last_attempt datetime NOT NULL default '0000-00-00 00:00:00',
+	date_created datetime NOT NULL default '0000-00-00 00:00:00',
+	PRIMARY KEY (account_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
--- setup/site_tables.sql
+++ setup/site_tables.sql
@@ -2,9 +2,45 @@
 use FishpoolDB;
 
 CREATE TABLE users (
-        account_id MEDIUMINT NOT NULL AUTO_INCREMENT,
-        name  TEXT,
+	account_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	name  VARCHAR(40),
 
-        FOREIGN KEY (id) REFERENCES accounts(account_id),
-        PRIMARY KEY (id)
+	FOREIGN KEY (id) REFERENCES accounts(account_id),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE groups (
+	group_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	owner MEDIUMINT NOT NULL,
+	name  VARCHAR(40),
+	date_created datetime NOT NULL default '0000-00-00 00:00:00',
+
+	FOREIGN KEY (owner) REFERENCES users(account_id),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE projects (
+	project_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	group MEDIUMINT NOT NULL,
+	name  VARCHAR(40),
+	date_created datetime NOT NULL default '0000-00-00 00:00:00',
+
+	FOREIGN KEY (group) REFERENCES groups(group_id),
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE files (
+	file_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	project MEDIUMINT NOT NULL,
+	-- maximum filename length on most OSs is 255
+	filename  VARCHAR(255),
+	-- maximum mime length (by spec) is 255
+	mime      VARCHAR(255),
+	date_added datetime NOT NULL default '0000-00-00 00:00:00',
+	date_modified datetime NOT NULL default '0000-00-00 00:00:00',
+
+	contents TEXT,
+
+	FOREIGN KEY (project) REFERENCES projects(project_id),
+	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;

94b10b23f2dfc2ececc82dcedfaa6b546addc457
Eric Dub&eacute <eric.alex.dube@gmail.com>
2015-11-30 02:30:31 -0500

    Modify login HTML to remove parent tags

---
+++ templates/login.template.php
@@ -0,0 +1,23 @@
+<div class="container">
+	<div style="outline:5px solid black;" class="Login-Box">
+		<form class="form-horizontal" role="form" method="POST">
+			<div class="form-group">
+				<label class="control-label col-xs-4" for="user">Username:</label>
+				<div class="col-xs-3">
+					<input type="text" class="form-control" id="user" placeholder="Enter username">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="control-label col-xs-4" for="pwd">Password:</label>
+				<div class="col-xs-3"> 
+				<input type="password" class="form-control" id="pwd" placeholder="Enter password">
+				</div>
+			</div>
+			<div class="form-group"> 
+				<div class="col-xs-offset-6 col-xs-1">
+					<input type="submit" class="btn btn-default">
+				</div>
+			</div>
+		</form>
+	</div>
+</div>

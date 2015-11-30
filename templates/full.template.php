55f884957b5f2d22f15e8145690b06e2b93c5c59
Eric Dub&eacute <eric.alex.dube@gmail.com>
2015-11-30 02:07:29 -0500

    Style main template

--- templates/full.template.php
+++ templates/full.template.php
@@ -1,12 +1,25 @@
 <html>
 <head>
 	<title><?php echo (isset($page_title)) ? "FP - ".$page_title : "Fishpool"; ?></title>
+	<!-- Nice sans-serif font from Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=PT+Sans+Caption' rel='stylesheet' type='text/css'>
+	<?php
+		HTMLShorts::includeCSS(WEB_PATH.'/resources/style/normalize.css');
+		HTMLShorts::includeCSS(WEB_PATH.'/resources/style/fp-header.css');
+		HTMLShorts::includeCSS(WEB_PATH.'/resources/style/fp-main.css');
+	?>
 </head>
 <body>
 	<div class="fp-header">
 		<div class="fp-title">
 			FishPool
 		</div>
+	</div>
+
+	<div class="fp-background-upper">
+	</div>
+
+	<div class="page-contents">
 	</div>
 </body>
 </html>

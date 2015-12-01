274e0ae40e620e2e82c9c233015938da945e2cba
Justin Mendes <100550776@oncampus.local>
2015-12-01 13:46:23 -0500

    Changed fonts to fishier ones

--- templates/register.template.php
+++ templates/register.template.php
@@ -11,55 +11,55 @@
 				<br />
 				<div class="form-group">
 					<label class="control-label col-xs-12 col-sm-2"
-					for="reg_name">Display Name:</label>
+					for="reg_name" style="font-family: 'PT Sans Caption', sans-serif;">Display Name:</label>
 
 					<div class="col-xs-12 col-sm-10">
 						<input type="text" name="name" class="form-control" id="reg_name"
-						placeholder="Enter an email">
+						placeholder="Enter an email" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<br />
 				<div class="form-group">
 					<label class="control-label col-xs-12 col-sm-2"
-					for="reg_email">Email:</label>
+					for="reg_email" style="font-family: 'PT Sans Caption', sans-serif;">Email:</label>
 
 					<div class="col-xs-12 col-sm-10">
 						<input type="text" name="email" class="form-control" id="reg_email"
-						placeholder="Enter an email">
+						placeholder="Enter an email" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<div class="form-group">
 					<label class="control-label col-xs-12 col-sm-2"
-					for="reg_repeat_email">Repeat:</label>
+					for="reg_repeat_email" style="font-family: 'PT Sans Caption', sans-serif;">Repeat:</label>
 
 					<div class="col-xs-12 col-sm-10">
 						<input type="text" class="form-control" id="reg_repeat_email"
-						placeholder="Enter an email">
+						placeholder="Enter an email" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<br />
 				<div class="form-group">
 					<label class="control-label col-xs-12 col-sm-2"
-					for="reg_pass">Password:</label>
+					for="reg_pass" style="font-family: 'PT Sans Caption', sans-serif;">Password:</label>
 					
 					<div class="col-xs-12 col-sm-10"> 
 						<input type="password" name="pass" class="form-control" id="reg_pass"
-						placeholder="Enter password">
+						placeholder="Enter password" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<div class="form-group">
 					<label class="control-label col-xs-12 col-sm-2"
-					for="reg_pass_repeat">Repeat:</label>
+					for="reg_pass_repeat" style="font-family: 'PT Sans Caption', sans-serif;">Repeat:</label>
 					
 					<div class="col-xs-12 col-sm-10"> 
 						<input type="password" class="form-control" id="reg_pass_repeat"
-						placeholder="Enter password">
+						placeholder="Enter password" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<br />
 				<div class="form-group"> 
 					<div class="col-sm-offset-2 col-xs-12 col-sm-10">
-						<input type="submit" class="btn btn-default" value="Register">
+						<input type="submit" class="btn btn-default" value="Register" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 			</form>

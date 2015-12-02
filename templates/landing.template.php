82dcef545fb2842b3e84084617760ecc80337b0c
unknown <100555757@UOSL15-R9029E3F.oncampus.local>
2015-12-02 15:48:16 -0500

    Fix column size on register box

--- templates/landing.template.php
+++ templates/landing.template.php
@@ -46,55 +46,55 @@
 			>
 				<br />
 				<div class="form-group">
-					<label class="control-label col-xs-12 col-sm-2"
+					<label class="control-label col-xs-12 col-sm-3"
 					for="reg_name" style="font-family: 'PT Sans Caption', sans-serif;">Display Name:</label>
 
-					<div class="col-xs-12 col-sm-10">
+					<div class="col-xs-12 col-sm-9">
 						<input type="text" name="name" class="form-control" id="reg_name"
 						placeholder="Enter an email" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<br />
 				<div class="form-group">
-					<label class="control-label col-xs-12 col-sm-2"
+					<label class="control-label col-xs-12 col-sm-3"
 					for="reg_email" style="font-family: 'PT Sans Caption', sans-serif;">Email:</label>
 
-					<div class="col-xs-12 col-sm-10">
+					<div class="col-xs-12 col-sm-9">
 						<input type="text" name="email" class="form-control" id="reg_email"
 						placeholder="Enter an email" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<div class="form-group">
-					<label class="control-label col-xs-12 col-sm-2"
+					<label class="control-label col-xs-12 col-sm-3"
 					for="reg_repeat_email" style="font-family: 'PT Sans Caption', sans-serif;">Repeat:</label>
 
-					<div class="col-xs-12 col-sm-10">
+					<div class="col-xs-12 col-sm-9">
 						<input type="text" class="form-control" id="reg_repeat_email"
 						placeholder="Enter an email" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<br />
 				<div class="form-group">
-					<label class="control-label col-xs-12 col-sm-2"
+					<label class="control-label col-xs-12 col-sm-3"
 					for="reg_pass" style="font-family: 'PT Sans Caption', sans-serif;">Password:</label>
 					
-					<div class="col-xs-12 col-sm-10"> 
+					<div class="col-xs-12 col-sm-9"> 
 						<input type="password" name="pass" class="form-control" id="reg_pass"
 						placeholder="Enter password" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<div class="form-group">
-					<label class="control-label col-xs-12 col-sm-2"
+					<label class="control-label col-xs-12 col-sm-3"
 					for="reg_pass_repeat" style="font-family: 'PT Sans Caption', sans-serif;">Repeat:</label>
 					
-					<div class="col-xs-12 col-sm-10"> 
+					<div class="col-xs-12 col-sm-9"> 
 						<input type="password" class="form-control" id="reg_pass_repeat"
 						placeholder="Enter password" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>
 				<br />
 				<div class="form-group"> 
-					<div class="col-sm-offset-2 col-xs-12 col-sm-10">
+					<div class="col-sm-offset-3 col-xs-12 col-sm-9">
 						<input type="submit" class="btn btn-default" value="Register" style="font-family: 'PT Sans Caption', sans-serif;">
 					</div>
 				</div>

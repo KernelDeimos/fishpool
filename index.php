0bad13ab6aa8e9967b71b33c6936da3745cc4d59
Justin Mendes <100550776@oncampus.local>
2015-12-02 22:21:11 -0500

    CodeUpDown Page created

--- index.php
+++ index.php
@@ -64,6 +64,10 @@ function main() {
 		$ex = new \Pages\LoginSubmit();
 		$ex->run();
 	}
+	else if ($page === "UpDownCode") {
+		$ex = new \Pages\UpDownCode();
+		$ex->run();
+	}
 	else {
 		$ex = new \Pages\TemplateTestPage();
 		$ex->run();

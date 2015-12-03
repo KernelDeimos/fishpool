25d774cab1405f9764d09c454e60665d9c8b1366
Eric Dub&eacute <eric.alex.dube@gmail.com>
2015-12-03 04:38:53 -0500

    Fix Request class

--- pages/TemplateTestPage.php
+++ pages/TemplateTestPage.php
@@ -6,9 +6,10 @@ use \Framework\ContentPage;
 class TemplateTestPage extends ContentPage {
 	function main($template) {
 		$contents = new \Framework\Template();
-		$contents->set_template_file(SITE_PATH."/templates/register.template.php");
+		$contents->set_template_file(SITE_PATH."/templates/simple_message.template.php");
 		$template->set_template_file(SITE_PATH."/templates/full.template.php");
 		$template->contents_template = $contents;
+		$contents->title = $this->request->get_page();
 		return ContentPage::PAGE_OKAY;
 	}
 }

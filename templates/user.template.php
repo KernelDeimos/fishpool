a00ac69cc94fda744d02a699c01c3045fc16b3d9
Evan Urquhart <100554128@oncampus.local>
2015-12-04 18:15:54 -0500

    add info fields to user database

--- templates/user.template.php
+++ templates/user.template.php
@@ -11,7 +11,7 @@
 
 
 <div class="container">
-	<div class="col-xs-12 col-lg-9">
+	<div class="col-xs-12 col-sm-6 col-md-8 col-lg-9">
 		<div class="fp-form-box">
 			<div class="form-header">
 				<div class="title"><?php echo $user_name . " bio";?>
@@ -22,30 +22,29 @@
 					<?php } ?>
 				</div>
 			</div>
-		</div>
-
+		</div>		
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
 		<?php if ($is_own_page) { ?>
 			<div class="fp-form-box">
 				<div class="form-header">
 					<div class="title">My Groups</div>
 				</div>
 
-				<div class="form-body">
-					I put the box here for demo; feel free to change it
+				<div class="form-body" style="font-family: 'PT Sans Caption', sans-serif;">
+					
 					<ul>
 						<?php if (isset($groups)) foreach ($groups as $group) { ?>
-							<li><?php echo $group->get_name(); ?></li>
+							<li style="font-family: 'PT Sans Caption', sans-serif;"><?php echo $group->get_name(); ?></li>
 						<?php } else { ?>
 							Error fetching groups! :/ [<?php echo $groups_fetch_error; ?>]
 						<?php } ?>
 					</ul>
 				</div>
-			</div>
+			
 
-			<div class="fp-form-box">
-				<div class="form-header">
-					<div class="title">Create New Group</div>
-				</div>
+	
+				 
 
 				<div class="form-body">
 					<form
@@ -55,23 +54,23 @@
 					data-success-url = "<?php echo $_SERVER['REQUEST_URI']; ?>"
 					>
 						<div class="form-group">
-							<label class="control-label col-xs-12 col-sm-2"
-							for="reg_name">Group Name:</label>
+							<label class="col-xs-12"
+							for="reg_name">Add New Group:</label>
 
-							<div class="col-xs-12 col-sm-10">
+							<div class="col-xs-12">
 								<input type="text" name="name" class="form-control" id="reg_name"
-								placeholder="The Code Group" style="font-family: 'PT Sans Caption', sans-serif;">
+								placeholder="Group Name" style="font-family: 'PT Sans Caption', sans-serif;">
 							</div>
 						</div>
 						<br />
 						<div class="form-group"> 
-							<div class="col-sm-offset-2 col-xs-12 col-sm-10">
+							<div class=" col-xs-12 ">
 								<input type="submit" class="btn btn-default" value="Create Group">
 							</div>
 						</div>
-					</form>
-				</div>
-			</div>
+					</form>				
+				</div></div>
+		
 		<?php } ?>
 	</div>
 </div>

62f8c36aa72ce706feef27540881d9d8b7a93257
Eric's Laptop <eric.alex.dube@gmail.com>
2015-12-04 17:15:19 -0500

    Add missing relation for projects and folders

--- setup/file_tables.sql
+++ setup/file_tables.sql
@@ -3,12 +3,14 @@ use FishpoolDB;
 
 CREATE TABLE IF NOT EXISTS folders (
 	folder_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	parent MEDIUMINT NOT NULL,
 	-- maximum filename length on most OSs is 255
 	name  VARCHAR(255),
 
 	date_added datetime NOT NULL default '0000-00-00 00:00:00',
 
 	FOREIGN KEY (project) REFERENCES projects(project_id),
+	FOREIGN KEY (parent)  REFERENCES folders(folder_id),
 	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
@@ -19,7 +21,7 @@ CREATE TABLE IF NOT EXISTS files (
 	filename  VARCHAR(255),
 	-- maximum mime length (by spec) is 255
 	mime      VARCHAR(255),
-	date_added datetime NOT NULL default '0000-00-00 00:00:00',
+	date_added datetime    NOT NULL default '0000-00-00 00:00:00',
 	date_modified datetime NOT NULL default '0000-00-00 00:00:00',
 
 	contents TEXT,
--- setup/generate_database.bat
+++ setup/generate_database.bat
@@ -1,5 +1,8 @@
 @echo off
+
 C:\xampp\mysql\bin\mysql -u root -p < accounts_table.sql
+
+C:\xampp\mysql\bin\mysql -u root -p < file_tables.sql
+
 C:\xampp\mysql\bin\mysql -u root -p < site_tables.sql
 C:\xampp\mysql\bin\mysql -u root -p < permissions_tables.sql
-C:\xampp\mysql\bin\mysql -u root -p < file_tables.sql
